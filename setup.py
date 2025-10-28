6ea771e5c6c638df7a83c2bf744296f6ff3c2905
Zumair Dabir <zumair.dabir@vit.edu.in>
2025-10-28 23:08:13 +0400

    Removed nested git repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative_AI_Project',
+    version='0.0.1',
+    author='Samia Sayyed',
+    author_email='samiasayyed123@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
